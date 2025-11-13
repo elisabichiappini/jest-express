db9c104b989ca04d47e55cb691da74785b6b09db
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-13 19:01:12 +0100

    featl: test semplici in jest

--- test/class.test.js
+++ test/class.test.js
@@ -7,3 +7,13 @@ test('creo istanza classe', () => {
     const user = new User();
     expect(user).toBeInstanceOf(User);
 });
+
+test('ccontrollo se il nome è Elisa', () => {
+    const user = new User();
+    expect(user.nome).toBe('Elisa');
+});
+
+test('controlla che User sia un oggetto', () => {
+    const user = new User();
+    expect(typeof user).toBe('object');
+}) 
